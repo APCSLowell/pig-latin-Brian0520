0e67ed63ca0102697596dc83d0eb57b6440ec83d
Brian0520 <143020505+Brian0520@users.noreply.github.com>
2024-01-26 10:01:22 -0800

    Update PigLatin.java

--- src/main/java/PigLatin.java
+++ src/main/java/PigLatin.java
@@ -41,20 +41,21 @@ public class PigLatin {
         //precondition: sWord is a valid String of length greater than 0
         //postcondition: returns the pig latin equivalent of sWord
 	   
-	    if(sWord.substring(0,2).equals("qu")) {
-		    return sWord.substring(2) + sWord.substring(0,2) + "ay";
-	}
-		else if(findFirstVowel(sWord) == 1){
-		    return sWord + "way";
-	    }
-	    else if(findFirstVowel(sWord) > 1){
-		  return sWord.substring(1) + sWord.charAt(0) + "ay";  
-	    }  
-	    else if(findFirstVowel(sWord) == -1) {
-		    return sWord + "ay";
-	    }
-	    else {
-		return "ERROR!";
-	    }
-    }
+	{
+      if(sWord.substring(0,2).equals("qu")) {
+        return sWord.substring(2) + sWord.substring(0,2) + "ay";
+      }
+      else if(findFirstVowel(sWord) == 0){
+        return sWord + "way";
+      }
+      else if(findFirstVowel(sWord) > 0){
+      return sWord.substring(findFirstVowel(sWord)) + sWord.substring(0,findFirstVowel(sWord)) + "ay";  
+      }  
+      else if(findFirstVowel(sWord) == -1) {
+        return sWord + "ay";
+      }
+      else {
+    return "ERROR!";
+      }
+}
 }//end PigLatin class
